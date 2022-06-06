6dec9606c5d7cde222f0c992d689cda7745845e9
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-06-06 12:13:14 +0100

    practice link, replace

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+
+import Link from 'next/link'
 
 export default function Home() {
+
+  const router = useRouter()
+  const handleClick = () => {
+    console.log('Placing your order')
+    // router.push('/product')
+    router.replace('/product')
+  }
   return (
     <div>
       <h1>Hello XXXX</h1>
+      <Link href="/blog">
+      <a>Blog</a>
+      </Link>
+      <Link href="/product">
+      <a>Products</a>
+      </Link>
+      <button onClick={handleClick}>
+        Place Holder
+      </button>
     </div>
   )
 }
